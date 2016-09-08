a276fbaea9bb3b1dbeab4c418734fc9f7ee34a83
Chyld Medford <chyld.medford@gmail.com>
2016-09-08 15:42:46 -0500

    wip

---
+++ compute/src/main/java/com/chyld/messaging/PositionQueueService.java
@@ -0,0 +1,28 @@
+package com.chyld.messaging;
+
+import com.chyld.entities.Position;
+import com.chyld.services.DeviceService;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+public class PositionQueueService {
+    private DeviceService service;
+
+    @Autowired
+    public void setService(DeviceService service) {
+        this.service = service;
+    }
+
+    @RabbitListener(queues = "fit.queue.pos")
+    public void receive(Message msg, HashMap<String, Object> data){
+        String key = msg.getMessageProperties().getReceivedRoutingKey();
+        String serial = (String) data.get("serial");
+        Position position = (Position) data.get("position");
+        service.addPosition(serial, position);
+    }
+}
---
+++ compute/src/main/java/com/chyld/messaging/RunQueueService.java
@@ -0,0 +1,8 @@
+package com.chyld.messaging;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RunQueueService {
+
+}
